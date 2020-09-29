3d44dcdf83f13a928642db4affd32bd1c1e0689d
Анастасия Елесина <elesina.anastasiya@bk.ru>
2020-09-29 21:04:04 +0400

    Практика 1 и 2

--- main.js
+++ main.js
@@ -14,4 +14,19 @@ function insertElement (classElement, ...children)
 // `${indicator} ${createElement('text', 'main','Команда выбрана')}<br>`,
 // `${indicator} ${createElement('text', 'main','Погода - ок')}<br>`
 // );
+/*Переключение прозрачности кнопок, работает,но не работает
+Кнопки действительно переключаются, но это происходит на старой странице,
+до перехода по щелчку, после перехода на др страницу проявляются изначальные
+настройки*/
+var menu = document.getElementById('menu');
 
+menu.onclick = function(event) {
+    let elementsImg = menu.querySelectorAll('img');
+    //Сделать все прозрачными
+    for (let img of elementsImg) { 
+        img.style.opacity = 0.75;
+    }
+    //Для элемента на который кликнули - в  полный цвет 
+    //event.target.style.opacity = 1;
+    event.target.className = "active";
+  };
