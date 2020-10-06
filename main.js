86310fefe55e7aa58dee0bf582dd1c6451b57cb7
Анастасия Елесина <elesina.anastasiya@bk.ru>
2020-10-06 20:09:51 +0400

    Practice 1-2 fix try 1

--- main.js
+++ main.js
@@ -2,16 +2,28 @@
 let menu = document.getElementById('menu');
 menu.addEventListener("click", function(event){
     // Обработка кнопок меню
-    let elementsImg = menu.querySelectorAll('img');
-    for (let img of elementsImg) { 
-        if (img.classList.contains("active")){
+    let activeImg = menu.querySelectorAll('img.active');
+    for (let img of activeImg) { 
             img.classList.remove("active");
             img.classList.add("inactive");
-        }
     }
+
     if (event.target.classList.contains("inactive")){
         event.target.classList.remove("inactive");
         event.target.classList.add("active");
     }
+
     // Обработка скрытия/появления страниц
+    let enableSection = document.querySelectorAll('section.enable');
+    //Скрываем все активные секции
+    for (let section of enableSection) { 
+            section.classList.remove("enable");
+            section.classList.add("disable");
+    }
+    //Активируем страницу
+    let classSection = `${event.target.id}-page`;
+    let activatedSection = document.querySelector(`.${classSection}`);
+    activatedSection.classList.remove("disable");
+    activatedSection.classList.add("enable");
+
 });
