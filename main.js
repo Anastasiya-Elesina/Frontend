bcd4eb2440ca2a28a0968a6bfca2cc5373667647
Анастасия Елесина <elesina.anastasiya@bk.ru>
2020-10-25 10:57:51 +0400

    Practice 3

--- main.js
+++ main.js
@@ -32,14 +32,14 @@ function findElement(selector, numOfElement=1){
     return document.querySelectorAll(`${selector}`)[numOfElement-1];
 }
 
-let name, speed, teamNumber, imgSrc;
+let name, speed, teamNumber, imgPath;
 let chosenShip = findElement('.chooseShip-page .section-inf');
 let curRocket;
 UpdateDataCard(1);//обновляем переменные чтобы они содержали актуальную информацию
 
 let buttonCreateShip = document.getElementById('button-create-rocket');
 buttonCreateShip.addEventListener("click", function(event){
-    curRocket = new Rocket (name, speed, teamNumber,imgSrc);
+    curRocket = new Rocket (name, speed, teamNumber, imgPath);
 });
 
 function UpdateDataCard(num){
@@ -48,14 +48,15 @@ function UpdateDataCard(num){
     name = shipCard.querySelector('ol li:nth-of-type(1) span:last-child').textContent;
     speed = shipCard.querySelector('ol li:nth-of-type(2) span:last-child').textContent;
     teamNumber = shipCard.querySelector('ol li:nth-of-type(3) span:last-child').textContent;
-    imgSrc = shipCard.querySelector('img').src;
+    // imgSrc = shipCard.querySelector('img').src; //Путь указан полностью, т.е. не будет работать на др компьтерах 
+    imgPath = `Materials/rocket-${num}.png`;
 }
 function UpdateViewCard(){
     //Меняем информацию в самом верхнем блоке в соответствии с номером карточки корабля
     chosenShip.querySelector('ol li:nth-of-type(1) span:last-child').textContent = name;;
     chosenShip.querySelector('ol li:nth-of-type(2) span:last-child').textContent = speed;
     chosenShip.querySelector('ol li:nth-of-type(3) span:last-child').textContent = teamNumber;
-    chosenShip.querySelector('img').src = imgSrc;
+    chosenShip.querySelector('img').src = imgPath;
 }
 //Обработка событий радио-кнопок: ловим события с помощью jQuery
 $('input[type="radio"]').on('change', function(event) {
